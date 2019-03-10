b45d5bc641340f43dafff611749ecbb31f3c039c
Gurditt Rai <raig@uoguelph.ca>
2019-03-10 17:52:10 -0400

    t2 working

--- fruit.py
+++ fruit.py
@@ -8,14 +8,6 @@ from matplotlib import pyplot as plt
 
 from itertools import product
 
-def cmpimgs(img1,img2):
-    plt.subplot(121),plt.imshow(img1,cmap = 'gray')
-    plt.title('Image 1'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img2,cmap = 'gray')
-    plt.title('Image 2'), plt.xticks([]), plt.yticks([])
-
-    plt.show()
-
 #Colour Dectection
 #Simple colour detection will be handled within this function.
 def colourDetect(img):
@@ -50,7 +42,7 @@ img = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 img = cv2.GaussianBlur(img,(9,9),cv2.BORDER_DEFAULT)
 
 # get circles
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,120,param1=50,param2=30, minRadius=100, maxRadius=250)
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,120,param1=50,param2=30,minRadius=50, maxRadius=150)
 # circles_round = np.uint16(np.around(circles))
 
 count = 0
@@ -70,22 +62,28 @@ for i in circles[0,:]:
     cropimg = image[y0:y1,x0:x1]
 
     # draw cropped image
-    cv2.imshow('detected circles', cropimg)
-    cv2.waitKey(0)
+    cv2.imshow('cropped circles', cropimg)
 
     # get red area
     redimg = colourDetect(cropimg)
+    cv2.imshow('color detection', redimg)
+    cv2.waitKey(0)
+
+    #threshold to make comparsion easier
+    ret,thresh1 = cv2.threshold(redimg,105,255,cv2.THRESH_BINARY)
+
+    # count and compare
     redcount = 0
     emptypixel = np.matrix('0 0 0')
     for i in xrange(redimg.shape[0]):
         for j in xrange(redimg.shape[1]):
             # get the pixel value
-            value = np.array_equal(np.matrix(redimg[i][j]), emptypixel)
-            # print value
-            if (value):
+            value = np.array_equal(np.matrix(thresh1[i][j]), emptypixel)
+            # remove background pixel from count
+            if (value == False):
                 redcount += 1
 
-    #threshold
+    #threshold for red
     total = redimg.shape[0] * redimg.shape[1]
     percent = (redcount*100) / total
 
@@ -93,6 +91,15 @@ for i in circles[0,:]:
     print percent
     if (percent > 35):
         count += 1
+
+for i in circles[0,:]:
+    # circle properties
+    center = (i[0],i[1])
+    radius = i[2]
+
+    # draw the outer circle
+    cv2.circle(image,center,radius,(0,255,0),2)
+    # count += 1
 
 print str(count) + ' circles'
 cv2.imshow('detected circles', image)
